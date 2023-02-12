c956db0feb5925a7fe4d5b50e084ce5cdbae7f48
kabutokunnDX <danieru.hakka@gmail.com>
2023-02-12 15:31:23 +0900

    うぇうぇうぇ

--- src/main/java/kbtdx/man10loginbonus/Commands.java
+++ src/main/java/kbtdx/man10loginbonus/Commands.java
@@ -4,7 +4,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import static kbtdx.man10loginbonus.Man10LoginBonus.mlb;
 
 public class Commands implements CommandExecutor {
     @Override
@@ -12,6 +19,29 @@ public class Commands implements CommandExecutor {
         if (!sender.hasPermission("mlb.user")){
             sender.sendMessage(ChatColor.RED + "[Man10LoginBonus] 権限がありません");
             return true;
+        }
+        if (sender.hasPermission("mlb.op")){
+            if (args.length==0)return true;
+            else {
+                if (args[0].equalsIgnoreCase("createbonus")){
+                    Player p = (Player) sender;
+                    File file = new File(mlb.getDataFolder().getAbsolutePath() + File.separator + "bonuses");
+                    File filee = new File(file, File.separator + args[1] + ".yml");
+                    YamlConfiguration yml = new YamlConfiguration();
+                    yml.set("Material",p.getInventory().getItemInMainHand().getType());
+                    yml.set("DisplayName",p.getInventory().getItemInMainHand().getItemMeta().getDisplayName());
+                    yml.set("CustomModelData",p.getInventory().getItemInMainHand().getItemMeta().getCustomModelData());
+                    yml.set("Lore",p.getInventory().getItemInMainHand().getItemMeta().getLore());
+                    yml.set("Modifiers",p.getInventory().getItemInMainHand().getItemMeta().getAttributeModifiers());
+                    yml.set("ItemFlags",p.getInventory().getItemInMainHand().getItemFlags());
+                    try {
+                        yml.save(filee);
+                        sender.sendMessage(ChatColor.BLUE + "§l[Man10LoginBonus]§f 保存しました。");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         }
         return true;
     }
--- src/main/java/kbtdx/man10loginbonus/Man10LoginBonus.java
+++ src/main/java/kbtdx/man10loginbonus/Man10LoginBonus.java
@@ -1,6 +1,9 @@
 package kbtdx.man10loginbonus;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class Man10LoginBonus extends JavaPlugin {
 
@@ -10,6 +13,8 @@ public final class Man10LoginBonus extends JavaPlugin {
     public void onEnable() {
         mlb=this;
         getCommand("mlb").setExecutor(new Commands());
+        File file = new File(mlb.getDataFolder().getAbsolutePath() + File.separator + "bonuses");
+        if (file.mkdir()){Bukkit.getLogger().info("ボーナスファイルを作成しました。");}else {Bukkit.getLogger().info("ボーナスファイルの作成に失敗しました。");}
         mysql=new MySQLManager(mlb,"mlb_data");
         mysql.execute("create table if not exists mlb_data(id int auto_increment,mcid varchar(16),uuid varchar(36),login_days int,end_login varchar(10))");
     }
